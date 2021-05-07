a46e81e5cfad5e75a80c4e6241b7c6a5da5bd2ad
tolgonayb <tolgonayb@gmail.com>
2021-05-07 11:08:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var name = prompt("Введите свое имя и фамилию", " ");
+alert("Привет, Евгений Киселев!");
+
+var number = prompt ("Введите 2 числа", "2, 5")
+console.log(2 < 5);
+console.log(2 <= 5);
+console.log(2 < 5);
+console.log(2 <= 5);
+
+var color = "red"
+switch (color) {
+    case "red":
+        console.log("Pedestrian should stop")
+        break;
+    case "yellow":
+        console.log("Pedestrian should wait")
+        break;
+    case "green":
+        console.log("Pedestrian should go")
+    default:
+        console.log("Pedestrian cross against the traffic lights")
+
+}
+
